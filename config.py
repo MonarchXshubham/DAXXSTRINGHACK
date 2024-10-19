eaab0111f83d97ea997d284a1e7649fdfc3d2bdf
MonarchXshubham <shubhamxmonarch@gmail.com>
2024-10-19 11:55:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ class Config:
     API_HASH = "4022d5686b9b7a7cf8891205921a0ab3"
     #TOKEN = "6521122303:AAGCO3XMjcA0SN5NAi1M0NpmbmMxEtwwYbg"
     TOKEN = os.environ.get("TOKEN", None)
-    MONGO_URL = "mongodb+srv://teamdaxx123:teamdaxx123@cluster0.ysbpgcp.mongodb.net/?retryWrites=true&w=majority"
+    MONGO_URL = "mongodb+srv://Cluster0:shubhamgaming33@cluster0.okmw2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     START_PIC = "https://telegra.ph/file/a8ba8edd60489a54f2f84.jpg"
     SUDOERS = filters.user(["7871556756"])
